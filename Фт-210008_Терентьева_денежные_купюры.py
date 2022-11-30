f96e000939c066919d6a8e0b8ea008a0aa0e16e9
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-11-30 17:31:51 +0500

    комментарии

--- Фт-210008_Терентьева_денежные_купюры.py
+++ Фт-210008_Терентьева_денежные_купюры.py
@@ -1,19 +1,18 @@
-def errorCheck(n):
+def errorCheck(n):                                                            #метод обработки ошибок 
     try:
         n = int(n)
     except Exception:
         return -1
     return n
 N = - 1
-while N == -1:
+while N == -1:                                                                   #цикл для ввода числа и обработка ошибок
     N = int(input('Введите натуральное число:\n'))
-    N = errorCheck(N)
     if N<=0:
         print('Ошибка! Необходимо ввести натуральное число\n')
         N=-1
-k=[64,32,16,8,4,2,1]
+k=[64,32,16,8,4,2,1]                                                                 #список используемых купюр
 print('Сумму можно выплатить:\n')
-for i in k:
+for i in k:                                                                          #поиск в списке нужных купюр и вывод их количества в консоль
     num=N//i
     N=N-(num*i)
     if num!=0:
