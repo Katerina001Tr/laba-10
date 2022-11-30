ae6580fc27f09339f5be1224b181ba93daafef9f
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-11-30 17:18:10 +0500

    обработка ошибок и ввод цикла

--- Фт-210008_Терентьева_денежные_купюры.py
+++ Фт-210008_Терентьева_денежные_купюры.py
@@ -4,3 +4,10 @@ def errorCheck(n):
     except Exception:
         return -1
     return n
+N = - 1
+while N == -1:
+    N = int(input('Введите натуральное число:\n'))
+    N = errorCheck(N)
+    if N<=0:
+        print('Ошибка! Необходимо ввести натуральное число\n')
+        N=-1
